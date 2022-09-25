a6511c898d0d6173bba33838d8f069f8f23a38a5
hector.gv00 <hector.gv00@gmail.com>
2022-09-25 18:13:17 +0200

    Paginas de google bing y netflix

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+let boton = document.getElementById("botonBuscar");
+
+boton.addEventListener("click", function(){
+    let texto = document.getElementById("mainInput").value;
+    alert("Esta no es la pagina de google jajaj no tengo ni idea sobre " + texto);})
+
+document.addEventListener("keydown", function(){
+    let texto = document.getElementById("mainInput").value;
+    alert("Esta no es la pagina de google jajaj no tengo ni idea sobre " + texto);
+})
